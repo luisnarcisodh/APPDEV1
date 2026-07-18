7db60e03bb1bca8a4513a6e381f21729623fe96a
Luis Narciso Huevos <luisnarciso.huevos0611@gmail.com>
2026-07-18 20:41:01 +0800

    Done with laboratory 3

---
+++ L03_getting-started-react/src/App.jsx
@@ -0,0 +1,40 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>About Me</h1>
+
+      <h2>Personal Information</h2>
+      <ul>
+        <li>
+          <strong>Full Name:</strong> Luis Narciso D. Huevos
+        </li>
+        <li>
+          <strong>Course and Year:</strong> Bachelor of Science in Information Systems - 3rd Year
+        </li>
+        <li>
+          <strong>Fun Fact:</strong> I love spending my free time hanging out at cafes and playing table tennis.
+        </li>
+      </ul>
+
+      <h2>My Reflection</h2>
+
+      <p>
+        Throughout this course, I gained valuable insights into utilizing Git and exploring various emerging technologies. I also developed the practical skill of building and maintaining a professional GitHub profile. Initially, I found navigating Git to be quite challenging, as version control concepts were new to me. However, through continuous practice and perseverance, I eventually grasped how it works and overcame that learning curve.
+      </p>
+
+      <p>
+       Looking ahead, I am confident that mastering these tools will be highly beneficial for my future career. I now realize how essential version control is for teamwork and efficiency. These skills will undoubtedly prepare me for seamless collaboration on real-world projects within the Information Systems field and the broader tech industry.
+      </p>
+
+      <footer>
+        <p>
+          <strong>Thank you for visiting my About Me page!</strong> 
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
